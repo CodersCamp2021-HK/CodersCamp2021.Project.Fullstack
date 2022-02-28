5abdd1b0c6e98126cfcd1bb165f94a3f849a5501
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-02-28 23:09:00 +0100

    add basic orders schema

---
+++ packages/server/src/orders/database/OrderSchema.ts
@@ -0,0 +1,71 @@
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+// import { Address } from '../addresses/database/AddressSchema';
+import { User } from '../../users/database/UserSchema';
+
+export type OrderDocument = Order & Document;
+
+const ORDER_CONSTANTS = Object.freeze({
+  COMMENT: Object.freeze({
+    MAX_LENGTH: 300,
+  }),
+  PRICE: Object.freeze({
+    MIN: 0,
+  }),
+});
+
+class SubOrder {
+  @Prop({ required: true })
+  deliveryDate: Date;
+
+  @Prop({ required: true })
+  hourStart: Date;
+
+  @Prop({ required: true })
+  hourEnd: Date;
+
+  @Prop({ default: false })
+  delivered: boolean;
+}
+
+@Schema({
+  collection: 'order',
+})
+class Order {
+  readonly id: string;
+
+  //   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }], required: true })
+  //   addressId: Address[];
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  userId: User;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop()
+  subOrders: SubOrder[];
+
+  @Prop(
+    raw({
+      dishId: {
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Dish' }],
+        required: true,
+      },
+      price: { type: Number, min: ORDER_CONSTANTS.PRICE.MIN },
+      count: { type: Number },
+      excludedIngredients: { type: [String] },
+    }),
+  )
+  dishes: object[];
+
+  @Prop({
+    maxlength: ORDER_CONSTANTS.COMMENT.MAX_LENGTH,
+  })
+  comment: string;
+}
+
+const OrderSchema = SchemaFactory.createForClass(Order);
+
+export { OrderSchema };
