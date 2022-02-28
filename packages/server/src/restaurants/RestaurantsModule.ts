9f3acd32a4872424bcf8b4086422c789e74996ed
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-02-28 15:58:13 +0100

    Add Dish controller #20 (#39)
    * feat(server): add partner dish controller
    
    * fix(server): restrict routes to partner
    
    * fix(server): fix ingredient list
    
    * feat(server): generate sdk
    
    * feat(server): add RestaurantDishController
    
    * feat(server): suppy partnerId to controller routes

--- packages/server/src/restaurants/RestaurantsModule.ts
+++ packages/server/src/restaurants/RestaurantsModule.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { Restaurant, RestaurantSchema } from './database';
+import { PartnerDishController } from './dishes/api/PartnerDishController';
+import { RestaurantDishController } from './dishes/api/RestaurantDishController';
 import { RestaurantsFacade } from './infra';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }])],
+  controllers: [RestaurantDishController, PartnerDishController],
   providers: [RestaurantsFacade],
   exports: [RestaurantsFacade],
 })
