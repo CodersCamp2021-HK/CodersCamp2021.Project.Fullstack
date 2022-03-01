3a1b88e154633d8ef290cbcdb7b1e38b539de04d
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-01 21:08:35 +0100

    fix(server): adjust DTO to restaurant changes

--- packages/server/src/restaurants/profile/api/PartnerProfileDto.ts
+++ packages/server/src/restaurants/profile/api/PartnerProfileDto.ts
@@ -6,12 +6,7 @@ import { ApiPhoneNumberProperty } from '../../../auth/api/decorators';
 import { RestaurantDto } from '../../api/RestaurantDto';
 import { RESTAURANT_CONSTANTS } from '../../database';
 
-/*
-TODO: once #41 gets merged, update:
-- type -> cuisineType
-- pick description from RestaurantDto using PickType instead of declaring here
-*/
-class PartnerProfileDto extends PickType(RestaurantDto, ['id', 'name', 'type', 'tags'] as const) {
+class PartnerProfileDto extends PickType(RestaurantDto, ['id', 'name', 'description', 'cuisineType', 'tags'] as const) {
   @ApiProperty({
     pattern: RESTAURANT_CONSTANTS.BANK_ACCOUNT_NUMBER.REGEX.source,
     example: '72920080748556126838146923',
@@ -27,12 +22,6 @@ class PartnerProfileDto extends PickType(RestaurantDto, ['id', 'name', 'type', '
     type: [AddressDto],
   })
   readonly addressId: AddressDto[];
-
-  @ApiProperty({
-    maxLength: RESTAURANT_CONSTANTS.DESCRIPTION.MAX_LENGTH,
-    example: 'Opis restauracji.',
-  })
-  readonly description: string;
 }
 
 class UpdatePartnerProfileDto extends OmitType(PartnerProfileDto, ['id'] as const) {}
