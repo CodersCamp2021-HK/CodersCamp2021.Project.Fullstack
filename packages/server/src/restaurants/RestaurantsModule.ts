09616d8515ceeec54055b775785bb23736a9dce1
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-02 00:25:05 +0100

    feat(server): add GetRestaurantHandler

--- packages/server/src/restaurants/RestaurantsModule.ts
+++ packages/server/src/restaurants/RestaurantsModule.ts
@@ -5,13 +5,14 @@ import { RestaurantController } from './api/RestaurantController';
 import { Restaurant, RestaurantSchema } from './database';
 import { PartnerDishController } from './dishes/api/PartnerDishController';
 import { RestaurantDishController } from './dishes/api/RestaurantDishController';
+import { GetRestaurantHandler } from './domain/GetRestaurantHandler';
 import { ListRestaurantsHandler } from './domain/ListRestaurantsHandler';
 import { RestaurantsFacade } from './infra';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }])],
   controllers: [RestaurantController, RestaurantDishController, PartnerDishController],
-  providers: [RestaurantsFacade, ListRestaurantsHandler],
+  providers: [RestaurantsFacade, GetRestaurantHandler, ListRestaurantsHandler],
   exports: [RestaurantsFacade],
 })
 class RestaurantsModule {}
