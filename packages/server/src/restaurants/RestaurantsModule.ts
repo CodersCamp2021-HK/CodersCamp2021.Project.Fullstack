037369aee06bfaede2cad5ea3d08ecc9d517a579
mmejer <44590421+mmejer@users.noreply.github.com>
2022-03-02 14:10:21 +0100

    feat(server): hook up CreateDishHandler

--- packages/server/src/restaurants/RestaurantsModule.ts
+++ packages/server/src/restaurants/RestaurantsModule.ts
@@ -5,13 +5,18 @@ import { RestaurantController } from './api/RestaurantController';
 import { Restaurant, RestaurantSchema } from './database';
 import { PartnerDishController } from './dishes/api/PartnerDishController';
 import { RestaurantDishController } from './dishes/api/RestaurantDishController';
+import { Dish, DishSchema } from './dishes/database';
+import { CreateDishHandler } from './dishes/domain';
 import { ListRestaurantsHandler } from './domain/ListRestaurantsHandler';
 import { RestaurantsFacade } from './infra';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }]),
+    MongooseModule.forFeature([{ name: Dish.name, schema: DishSchema }]),
+  ],
   controllers: [RestaurantController, RestaurantDishController, PartnerDishController],
-  providers: [RestaurantsFacade, ListRestaurantsHandler],
+  providers: [RestaurantsFacade, ListRestaurantsHandler, CreateDishHandler],
   exports: [RestaurantsFacade],
 })
 class RestaurantsModule {}
