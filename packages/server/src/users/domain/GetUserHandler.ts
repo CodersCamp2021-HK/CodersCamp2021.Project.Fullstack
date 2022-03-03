f4282e1af445088c0f077816aed92f4934d9cfe7
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-03-03 08:19:21 +0100

    add basic structure for User GET

---
+++ packages/server/src/users/domain/GetUserHandler.ts
@@ -0,0 +1,22 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { plainToInstance } from 'class-transformer';
+import { Model } from 'mongoose';
+
+import { Handler } from '../../shared';
+import { User, UserDocument } from '../database';
+
+interface GetUserRequest {
+  readonly userId: string;
+}
+
+class GetUserHandler implements Handler<GetUserRequest, User | null> {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async exec(req: GetUserRequest): Promise<User | null> {
+    const user = await this.userModel.findById(req.userId);
+    if (!user) return null;
+    return plainToInstance(User, user);
+  }
+}
+
+export { GetUserHandler };
