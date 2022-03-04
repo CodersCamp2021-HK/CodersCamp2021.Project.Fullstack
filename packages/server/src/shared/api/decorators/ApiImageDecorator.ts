14133eb3ef115c4ceff50b147b92cb2966024b53
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-04 10:21:38 +0100

    feat(api): add image url schema

--- packages/server/src/shared/api/decorators/ApiImageDecorator.ts
+++ packages/server/src/shared/api/decorators/ApiImageDecorator.ts
@@ -4,9 +4,15 @@ import { Transform } from 'class-transformer';
 
 import { ImageType } from '../../../image/shared';
 
+const IMAGE_URL_SCHEMA = Object.freeze({
+  type: 'string',
+  pattern: /\/api\/img\/(restaurant|dish)\/[0-9a-fA-F]{24}$/.source,
+  example: '/api/img/restaurant/6200218668fc82e7bdf15088',
+});
+
 function ApiImage(type: ImageType): PropertyDecorator {
   return applyDecorators(
-    ApiProperty({ required: false }),
+    ApiProperty({ required: false, ...IMAGE_URL_SCHEMA }),
     Transform(({ obj, value }) => (value ? `/api/img/${type}/${obj.id}` : undefined)),
   );
 }
