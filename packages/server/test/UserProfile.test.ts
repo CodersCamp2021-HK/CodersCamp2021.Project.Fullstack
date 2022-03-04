073466e3845574fb274a8d1fc0bcb9ddc7290854
jskrajna <jskrajna@gmail.com>
2022-03-04 14:38:30 +0100

    Issue 43 add handler + tests   get user profile (#63)
    * add basic structure for User GET
    
    * add users GET handler
    
    * add handler for GET and test
    
    * Update packages/server/src/users/domain/GetUserHandler.ts
    
    Co-authored-by: Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
    
    * Update packages/server/test/UserProfile.test.ts
    
    Co-authored-by: Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
    
    * Update packages/server/test/UserProfile.test.ts
    
    Co-authored-by: Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
    
    Co-authored-by: Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
    Co-authored-by: Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>

---
+++ packages/server/test/UserProfile.test.ts
@@ -0,0 +1,32 @@
+import { HttpStatus } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { Role } from '../src/shared';
+import { initE2eFixture } from './E2eFixture';
+import { accessTokenAsCookie } from './shared';
+
+const PATH = '/api/users/profile';
+
+describe(`${PATH}`, () => {
+  const fixture = initE2eFixture();
+
+  afterEach(async () => {
+    await fixture.db.userModel.deleteMany();
+  });
+
+  it('GET /', async () => {
+    // Given
+    const user = {};
+
+    const created = await fixture.db.userModel.create(user);
+    const id = created._id?.toString();
+    const accessToken = accessTokenAsCookie(fixture.app.get(JwtService).sign({ role: Role.User, sub: id }));
+
+    // When
+    const resp = await fixture.agent().get(PATH).set('Cookie', [accessToken]).send();
+
+    // Then
+    expect(resp.status).toBe(HttpStatus.OK);
+    expect(created).toEqual(expect.objectContaining(resp.body));
+  });
+});
