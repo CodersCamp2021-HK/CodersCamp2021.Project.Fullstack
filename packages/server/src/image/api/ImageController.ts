006c134f269b1f71d193323d22d54e1fd1bbb17e
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-04 12:51:27 +0100

    feat(server): implement file upload POC

--- packages/server/src/image/api/ImageController.ts
+++ packages/server/src/image/api/ImageController.ts
@@ -1,6 +1,7 @@
-import { Param, Res, StreamableFile } from '@nestjs/common';
-import { ApiParam } from '@nestjs/swagger';
-import { Response } from 'express';
+import { Param, Post, Res, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiParam } from '@nestjs/swagger';
+import { Express, Response } from 'express';
 
 import { ApiController, ApiGet, ApiObjectIdParam } from '../../shared';
 import { GetImageHandler } from '../domain/GetImageHandler';
@@ -19,6 +20,24 @@ class ImageController {
 
     res.set({ 'Content-Type': fileData.contentType, 'Content-Disposition': 'inline' });
     return new StreamableFile(fileData.buffer);
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('file')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
 
