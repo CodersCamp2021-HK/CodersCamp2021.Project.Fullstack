59906343fd85ab9b0c4dcf8deb4116ef07104eae
mmejer <44590421+mmejer@users.noreply.github.com>
2022-03-08 20:12:32 +0100

    refactor(test): delete POST dish test

--- packages/server/test/Dishes.test.ts
+++ packages/server/test/Dishes.test.ts
@@ -1,14 +1,9 @@
 import { HttpStatus } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 
-import { CreateDishDto } from '../src/restaurants/dishes/api/DishDto';
-import { Role } from '../src/shared';
 import { dishDto } from './ApiDtoUtils';
 import { initE2eFixture } from './E2eFixture';
-import { accessTokenAsCookie } from './shared';
 
-const PATH = '/api/partner/dishes';
-const PATH_ALL_DISHES = '/api/dishes';
+const PATH = '/api/dishes';
 
 describe(`${PATH}`, () => {
   const fixture = initE2eFixture();
@@ -23,23 +18,10 @@ describe(`${PATH}`, () => {
     await fixture.db.dishModel.create(dishes);
 
     // When
-    const res = await fixture.req.get(PATH_ALL_DISHES);
+    const res = await fixture.req.get(PATH);
 
     // Then
     expect(res.status).toBe(HttpStatus.OK);
     expect(res.body.data).toHaveLength(dishes.length);
-  });
-
-  it('POST /', async () => {
-    // Given
-    const accessToken = accessTokenAsCookie(fixture.app.get(JwtService).sign({ role: Role.Partner }));
-    const reqBody: CreateDishDto = dishDto();
-
-    // When
-    const res = await fixture.agent().post(PATH).set('Cookie', [accessToken]).send(reqBody);
-
-    // Then
-    expect(res.status).toBe(HttpStatus.CREATED);
-    expect(res.body).toEqual(expect.objectContaining(reqBody));
   });
 });
