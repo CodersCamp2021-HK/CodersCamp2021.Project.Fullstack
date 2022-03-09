124aa8be531baed8e4107f2eb9030c5ec242f5dc
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-03-09 21:22:45 +0100

    Remove Articles #71  (#73)
    * refactor: delete articles from api
    
    * fix(server): fix interface name duplication in SDK
    
    * fix(client): fix client tests
    
    * refactor(server): fix a comment

--- packages/sdk/src/apis/index.ts
+++ packages/sdk/src/apis/index.ts
@@ -1,3 +1,14 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './ArticlesApi';
+export * from './AddressesApi';
+export * from './AllDishesApi';
+export * from './AuthApi';
+export * from './ImagesApi';
+export * from './OrdersApi';
+export * from './PartnersDishesApi';
+export * from './PartnersProfileApi';
+export * from './RestaurantsApi';
+export * from './RestaurantsDishesApi';
+export * from './UsersFavouriteDishesApi';
+export * from './UsersFavouriteRestaurantsApi';
+export * from './UserssProfileApi';
