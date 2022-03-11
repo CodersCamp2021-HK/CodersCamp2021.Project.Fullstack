c26718d9c8b4dbb7e28deceb6363de6a702780de
Kamil <37578394+KamilDudek@users.noreply.github.com>
2022-03-11 18:20:54 +0100

    Issue 44 add handler + tests   edit user profile (#68)
    * feat: add UpdateUserHandler, modify Module and Controller
    
    * feat: add all fields from schema to update
    
    * feat: change UserDto
    
    * feat: change ApiProperty to Optional, improve card object
    
    * feat: add example to request body
    
    * feat: change address in userDto
    
    * bug: delete handler from userhandler
    
    * refactor: delete unused code
    
    * feat: delete all ref from UserDto
    
    * feat: delete all ref from UpdateHandler
    
    * feat: delete address
    
    * feat: update Handler
    
    * test: add put test
    
    * docs: add user endpoints to user.http
    
    * bug: add missing colones
    
    * refactor: change comments
    
    * refactor: change ApiProperty for Email and PhoneNumber
    
    * feat: change to findByIdandUpdate
    
    * feat: add CardDto, change expireDate to string with pattern
    
    * bug: add optional parameter for decorator email and phoneNumber
    
    * fix: card as a optional
    
    * feat/test: delete email, add test

--- packages/server/src/users/api/UserDto.ts
+++ packages/server/src/users/api/UserDto.ts
@@ -1,35 +1,59 @@
 import { ApiProperty, ApiPropertyOptional, OmitType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
-import { FavouriteRestaurantDto } from '../../restaurants/api/RestaurantDto';
-import { FavouriteDishDto } from '../../restaurants/dishes/api/DishDto';
+import { ApiPhoneNumberProperty } from '../../auth/api/decorators';
 import { ApiObjectIdProperty } from '../../shared';
+import { USER_CONSTANTS } from './../database';
 
+class CardDto {
+  @ApiPropertyOptional({
+    pattern: USER_CONSTANTS.CARD.NUMBER.REGEX.source,
+    example: '4562574783836030',
+  })
+  readonly number: string;
+
+  @ApiPropertyOptional({
+    pattern: USER_CONSTANTS.CARD.EXPIRATION_DATE,
+    example: '2022-10-12',
+  })
+  readonly expirationDate: string;
+
+  @ApiPropertyOptional({
+    minLength: USER_CONSTANTS.CARD.CVC.MIN_LENGTH,
+    maxLength: USER_CONSTANTS.CARD.CVC.MAX_LENGTH,
+    example: '722',
+  })
+  readonly securityCode: string;
+}
 class UserDto {
   @ApiObjectIdProperty()
   readonly id: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    minLength: USER_CONSTANTS.NAME.MIN_LENGTH,
+    maxLength: USER_CONSTANTS.NAME.MAX_LENGTH,
+    example: 'Jan',
+  })
   readonly name: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    minLength: USER_CONSTANTS.NAME.MIN_LENGTH,
+    maxLength: USER_CONSTANTS.NAME.MAX_LENGTH,
+    example: 'Kowalski',
+  })
   readonly surname: string;
 
-  @ApiPropertyOptional()
+  @ApiPhoneNumberProperty({ required: false })
   readonly phoneNumber: string;
 
-  @ApiPropertyOptional()
-  readonly addressId: string[];
-
-  @ApiProperty({ type: [FavouriteRestaurantDto] })
-  readonly favouriteRestaurants: FavouriteRestaurantDto[];
-
-  @ApiProperty({ type: [FavouriteDishDto] })
-  readonly favouriteDishes: FavouriteDishDto[];
-
-  @ApiProperty()
-  readonly orders: string[];
-
+  @Type(() => CardDto)
   @ApiProperty({
+    required: false,
+    type: CardDto,
+  })
+  readonly card: CardDto;
+
+  @ApiPropertyOptional({
     default: false,
   })
   readonly profileCompleted: boolean;
---
+++ packages/server/src/users/domain/UpdateUserProfileHandler.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Handler } from '../../shared';
+import { Card, User, UserDocument } from '../database';
+
+interface UpdateUserProfileRequest {
+  readonly id: string;
+  readonly name: string;
+  readonly surname: string;
+  readonly phoneNumber: string;
+  readonly card: Card;
+  readonly profileCompleted: boolean;
+}
+
+@Injectable()
+class UpdateUserProfileHandler implements Handler<UpdateUserProfileRequest, null | undefined> {
+  constructor(@InjectModel(User.name) private userModule: Model<UserDocument>) {}
+
+  async exec(req: UpdateUserProfileRequest): Promise<null | undefined> {
+    const result = await this.userModule.findByIdAndUpdate(req.id, {
+      name: req.name,
+      surname: req.surname,
+      phoneNumber: req.phoneNumber,
+      card: req.card,
+    });
+
+    //TODO update profileCompleted when all date are passed
+    // if (
+    //   req.name === undefined ||
+    //   req.surname === undefined ||
+    //   req.email === undefined ||
+    //   req.phoneNumber === undefined ||
+    //   req.card === undefined
+    // ) {
+    //   await this.userModule.findOneAndUpdate(
+    //     { _id: req.id },
+    //     {
+    //       profileCompleted: false,
+    //     },
+    //   );
+    // }
+
+    if (result === null) return null;
+
+    return undefined;
+  }
+}
+
+export { UpdateUserProfileHandler };
