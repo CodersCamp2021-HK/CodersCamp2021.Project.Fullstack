756cec3754913f5383088412e895d18a73d82c1f
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2022-03-11 12:18:25 +0100

    Add handler + tests - confirm the order #57  (#75)
    * feat(server): add CreateOrderHandler
    
    * fix(server): serialization and test fixes
    
    * refactor(server): change OrderDto and Schema fields
    
    * test(server): fix order test

--- packages/server/src/orders/api/OrderController.ts
+++ packages/server/src/orders/api/OrderController.ts
@@ -3,10 +3,13 @@ import { plainToInstance } from 'class-transformer';
 import { Response } from 'express';
 
 import { ApiAuthorization, ApiController, ApiCreate, Role, Url, UserId } from '../../shared';
+import { CreateOrderHandler } from '../domain';
 import { CreateOrderDto, OrderDto } from './OrderDto';
 
 @ApiController({ path: 'orders', name: 'Orders', description: 'Submitting an order' })
 class OrderController {
+  constructor(private readonly createOrderHandler: CreateOrderHandler) {}
+
   @ApiCreate({ name: 'order', response: OrderDto })
   @ApiAuthorization(Role.User)
   async create(
@@ -15,7 +18,7 @@ class OrderController {
     @Res({ passthrough: true }) res: Response,
     @Url() url: URL,
   ) {
-    const order = { ...createOrderDto, id: '6200218668fc82e7bdf15088', date: new Date(), userId }; // TODO: Hook up CreateOrderHandler
+    const order = await this.createOrderHandler.exec({ ...createOrderDto, userId });
     res.setHeader('Location', `${url.href}/${order.id}`);
     return plainToInstance(OrderDto, order);
   }
---
+++ packages/server/src/orders/domain/CreateOrderHandler.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { plainToInstance } from 'class-transformer';
+import { Model } from 'mongoose';
+
+import { Handler } from '../../shared';
+import { Order, OrderDocument } from '../database';
+
+interface CreateOrderRequest {
+  addressId: string;
+  userId: string;
+  subOrders: {
+    deliveryDate: Date;
+    hourStart: number;
+    hourEnd: number;
+    dishes: {
+      dishId: string;
+      count?: number;
+      excludedIngredients?: string[];
+    }[];
+  }[];
+  comment?: string;
+}
+
+@Injectable()
+class CreateOrderHandler implements Handler<CreateOrderRequest, Order> {
+  constructor(@InjectModel(Order.name) private orderModel: Model<OrderDocument>) {}
+
+  async exec(req: CreateOrderRequest): Promise<Order> {
+    const created = await this.orderModel.create({ ...req, date: new Date() });
+    return plainToInstance(Order, created);
+  }
+}
+
+export { CreateOrderHandler };
