23d2f2c44afe93ab0e65bd4be881a18a7dfe22cf
Justyna S <justyna.skrajna@aliso.pl>
2022-03-11 18:33:23 +0100

    fix lint

--- packages/server/src/restaurants/dishes/domain/DeleteDishHandler.ts
+++ packages/server/src/restaurants/dishes/domain/DeleteDishHandler.ts
@@ -3,8 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Handler } from '../../../shared';
-import { Dish, DishDocument } from '../database';
 import { Restaurant, RestaurantDocument } from '../../database';
+import { Dish, DishDocument } from '../database';
 
 interface DeleteDishRequest {
   readonly id: string;
@@ -14,14 +14,12 @@ interface DeleteDishRequest {
 @Injectable()
 class DeleteDishHandler implements Handler<DeleteDishRequest, null | undefined> {
   constructor(
-      @InjectModel(Dish.name) private dishModel: Model<DishDocument>,
-      @InjectModel(Restaurant.name) private restaurantModel: Model<RestaurantDocument>) {}
+    @InjectModel(Dish.name) private dishModel: Model<DishDocument>,
+    @InjectModel(Restaurant.name) private restaurantModel: Model<RestaurantDocument>,
+  ) {}
 
   async exec(req: DeleteDishRequest): Promise<null | undefined> {
-    await this.restaurantModel.updateOne(
-        { _id: req.restaurantId},
-        { $pull: {dishes: req.id } }
-    )
+    await this.restaurantModel.updateOne({ _id: req.restaurantId }, { $pull: { dishes: req.id } });
 
     const result = await this.dishModel.deleteOne({ _id: req.id, restaurant: req.restaurantId });
     if (result.deletedCount !== 1) return null;
