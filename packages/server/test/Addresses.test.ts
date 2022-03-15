119794c094c15398966c6b7e8c3410c4ea15fe3e
mmejer <44590421+mmejer@users.noreply.github.com>
2022-03-15 12:47:17 +0100

    test(server): add test for deleting address from list

--- packages/server/test/Addresses.test.ts
+++ packages/server/test/Addresses.test.ts
@@ -70,4 +70,30 @@ describe(`${USER_PATH} & ${PARTNER_PATH}`, () => {
     expect(res.status).toBe(HttpStatus.CREATED);
     expect(res.body).toEqual(expect.objectContaining(reqBody));
   });
+
+  it.each(ENDPOINT_ROLES)('PUT / (role:$role) (path:$path)', async ({ role, path }) => {
+    // Given
+    const owner = new ObjectId().toString();
+    const agent = fixture.agent(role, owner);
+    const addresses = [
+      { ...addressDto(), owner },
+      { ...addressDto(), owner },
+      { ...addressDto(), owner },
+    ];
+    const addressesList = await fixture.db.addressModel.create(addresses);
+    const addressId = addressesList[0]._id;
+
+    // When
+    const res0 = await agent.put(`${path}/${addressId}`);
+
+    // Then
+    expect(res0.status).toBe(HttpStatus.NO_CONTENT);
+
+    // When
+    const res1 = await agent.get(`${path}/`);
+
+    // Then
+    expect(res1.status).toBe(HttpStatus.OK);
+    expect(res1.body.data).toHaveLength(addresses.length - 1);
+  });
 });
