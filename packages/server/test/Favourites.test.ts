8360ec97b1c895dcdf03290837d1baf24acf1f29
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-19 12:56:36 +0100

    test(server): test favourite GET /dish

--- packages/server/test/Favourites.test.ts
+++ packages/server/test/Favourites.test.ts
@@ -1,8 +1,10 @@
 import { HttpStatus } from '@nestjs/common';
-import { ObjectId } from 'mongodb';
+import { plainToInstance } from 'class-transformer';
+import _ from 'lodash';
 
+import { FavouriteDishDto } from '../src/restaurants/dishes/api/DishDto';
 import { Role } from '../src/shared';
-import { initE2eFixture } from './shared';
+import { dishDto, initE2eFixture } from './shared';
 
 const PATH = '/api/users/favourite';
 
@@ -19,12 +21,21 @@ describe(`${PATH}`, () => {
   });
 
   it('GET /dishes', async () => {
+    // Given
+    const createdDishes = await fixture.db.dishModel.create(
+      _.times(5, () => dishDto({ restaurant: '6200218668fc82e7bdf15088' })),
+    );
+    const formattedDishes = createdDishes.map((dish) => plainToInstance(FavouriteDishDto, dish));
+    const createdUser = await fixture.db.userModel.create({
+      favouriteDishes: createdDishes.map((dish) => dish.id),
+    });
+    const agent = fixture.agent(Role.User, createdUser.id);
+
     // When
-    const owner = new ObjectId().toString();
-    const agent = fixture.agent(Role.User, owner);
     const resp = await agent.get(`${PATH}/dishes`);
 
     // Then
     expect(resp.status).toBe(HttpStatus.OK);
+    expect(resp.body.data).toEqual(formattedDishes);
   });
 });
