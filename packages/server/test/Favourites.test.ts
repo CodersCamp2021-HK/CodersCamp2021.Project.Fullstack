cb241921c4f22d9a21f04b3fc66cf3ef75411fa1
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-19 13:18:48 +0100

    test(server): test favourite PUT /dishes/:id

--- packages/server/test/Favourites.test.ts
+++ packages/server/test/Favourites.test.ts
@@ -1,12 +1,14 @@
 import { HttpStatus } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import _ from 'lodash';
+import { ObjectId } from 'mongodb';
 
 import { FavouriteDishDto } from '../src/restaurants/dishes/api/DishDto';
 import { Role } from '../src/shared';
 import { dishDto, initE2eFixture } from './shared';
 
 const PATH = '/api/users/favourite';
+const RESTAURANT_ID = '6200218668fc82e7bdf15088';
 
 describe(`${PATH}`, () => {
   const fixture = initE2eFixture();
@@ -22,9 +24,7 @@ describe(`${PATH}`, () => {
 
   it('GET /dishes', async () => {
     // Given
-    const createdDishes = await fixture.db.dishModel.create(
-      _.times(5, () => dishDto({ restaurant: '6200218668fc82e7bdf15088' })),
-    );
+    const createdDishes = await fixture.db.dishModel.create(_.times(5, () => dishDto({ restaurant: RESTAURANT_ID })));
     const formattedDishes = createdDishes.map((dish) => plainToInstance(FavouriteDishDto, dish));
     const createdUser = await fixture.db.userModel.create({
       favouriteDishes: createdDishes.map((dish) => dish.id),
@@ -37,5 +37,24 @@ describe(`${PATH}`, () => {
     // Then
     expect(resp.status).toBe(HttpStatus.OK);
     expect(resp.body.data).toEqual(formattedDishes);
+  });
+
+  it('PUT /dishes/:id', async () => {
+    // Given
+    const createdDish = await fixture.db.dishModel.create(dishDto({ restaurant: RESTAURANT_ID }));
+    const formattedDish = plainToInstance(FavouriteDishDto, createdDish);
+    const dishId = formattedDish.id;
+    const createdUser = await fixture.db.userModel.create({});
+    const agent = fixture.agent(Role.User, createdUser._id?.toString());
+    const fakeDishId = new ObjectId().toString();
+
+    // When
+    const resValid = await agent.put(`${PATH}/dishes/${dishId}`);
+    const resInvalid = await agent.put(`${PATH}/dishes/${fakeDishId}`);
+
+    // Then
+    expect(resValid.status).toBe(HttpStatus.OK);
+    expect(resValid.body).toEqual(formattedDish);
+    expect(resInvalid.status).toBe(HttpStatus.NOT_FOUND);
   });
 });
