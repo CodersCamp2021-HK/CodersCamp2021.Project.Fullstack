f410065d79aec486b2128a9de670352dae800c22
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-03-19 20:27:08 +0100

    remove from passed parameters whole docs and add proper fields instead

--- packages/server/src/mail/AuthMailService.ts
+++ packages/server/src/mail/AuthMailService.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
-import { Auth } from '../auth/database';
-
 @Injectable()
 export class AuthMailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendUserConfirmation(user: Auth, token: string) {
+  async sendUserConfirmation(email: string, token: string) {
     const url = `exampleurl.com?token=${token}`;
 
     await this.mailerService.sendMail({
-      to: user.email,
+      to: email,
       from: '"Support Team" <support@example.com>',
       subject: 'Gratulacje! Potwierdź swój email.',
       template: 'templates/user_confirmation',
       context: {
-        name: user.email,
+        name: email,
         url,
       },
     });
--- packages/server/src/mail/OrderMailService.ts
+++ packages/server/src/mail/OrderMailService.ts
@@ -1,22 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
-import { Auth } from '../auth/database';
-import { Order } from '../orders/database';
-
 @Injectable()
 export class OrderMailService {
   constructor(private mailerService: MailerService) {}
 
-  async sendOrderConfirmation(user: Auth, order: Order) {
+  async sendOrderConfirmation(email: string, orderId: string) {
     await this.mailerService.sendMail({
-      to: user.email,
+      to: email,
       from: '"Support Team" <support@example.com>',
       subject: 'Gratulacje! Złożyłeś zamówienie.',
       template: 'templates/order_confirmation',
       context: {
-        name: user.email,
-        orderNumber: order.id,
+        name: email,
+        orderNumber: orderId,
       },
     });
   }
