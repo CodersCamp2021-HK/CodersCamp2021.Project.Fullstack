d4e6cc14c59039276504a5a82c8d52829b2883fd
mmejer <44590421+mmejer@users.noreply.github.com>
2022-03-20 14:58:42 +0100

    test(server): add tests for adding, removing and receiving restaurants

--- packages/server/test/Favourites.test.ts
+++ packages/server/test/Favourites.test.ts
@@ -1,6 +1,11 @@
 import { HttpStatus } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import _ from 'lodash';
+import { ObjectId } from 'mongodb';
 
-import { initE2eFixture } from './shared';
+import { FavouriteRestaurantDto } from '../src/restaurants/api/RestaurantDto';
+import { Role } from '../src/shared';
+import { initE2eFixture, restaurantDto } from './shared';
 
 const PATH = '/api/users/favourite';
 
@@ -8,12 +13,78 @@ describe(`${PATH}`, () => {
   const fixture = initE2eFixture();
 
   it('GET /restaurants', async () => {
+    // Given
+    const createdRestaurants = await fixture.db.restaurantModel.create(_.times(5, () => restaurantDto()));
+    const formattedRestaurants = createdRestaurants.map((restaurant) =>
+      plainToInstance(FavouriteRestaurantDto, restaurant),
+    );
+    const createdUser = await fixture.db.userModel.create({
+      favouriteRestaurants: createdRestaurants.map((restaurant) => restaurant.id),
+    });
+    const agent = fixture.agent(Role.User, createdUser.id);
+
     // When
-    const resp = await fixture.req.get(`${PATH}/restaurants`);
+    const res = await agent.get(`${PATH}/restaurants`);
 
     // Then
-    expect(resp.status).toBe(HttpStatus.OK);
-    expect(resp.body.data).toHaveLength(0);
+    expect(res.status).toBe(HttpStatus.OK);
+    expect(res.body.data).toEqual(formattedRestaurants);
+  });
+
+  it('PUT /restaurants/:id', async () => {
+    // Given
+    const createdRestaurant = await fixture.db.restaurantModel.create(restaurantDto());
+    const formattedRestaurant = plainToInstance(FavouriteRestaurantDto, createdRestaurant);
+    const restaurantId = formattedRestaurant.id;
+    const createdUser = await fixture.db.userModel.create({});
+    const agent = fixture.agent(Role.User, createdUser.id);
+    const fakeRestaurantId = new ObjectId().toString();
+
+    // When
+    const resValid = await agent.put(`${PATH}/restaurants/${restaurantId}`);
+
+    // Then
+    expect(resValid.status).toBe(HttpStatus.OK);
+    expect(resValid.body).toEqual(formattedRestaurant);
+
+    // When
+    const resCheck = await agent.get(`${PATH}/restaurants`);
+
+    // Then
+    expect(resCheck.status).toBe(HttpStatus.OK);
+    expect(resCheck.body.data).toEqual(expect.arrayContaining([formattedRestaurant]));
+
+    // When
+    const resInvalid = await agent.put(`${PATH}/restaurants/${fakeRestaurantId}`);
+
+    // Then
+    expect(resInvalid.status).toBe(HttpStatus.NOT_FOUND);
+  });
+
+  it('DELETE /restaurants/:id', async () => {
+    // Given
+    const createdRestaurant = await fixture.db.restaurantModel.create(restaurantDto());
+    const createdUser = await fixture.db.userModel.create({ favouriteRestaurants: [createdRestaurant] });
+    const agent = fixture.agent(Role.User, createdUser.id);
+
+    // When
+    const res0 = await agent.delete(`${PATH}/restaurants/${createdRestaurant.id}`);
+
+    // Then
+    expect(res0.status).toBe(HttpStatus.NO_CONTENT);
+
+    // When
+    const res1 = await agent.delete(`${PATH}/restaurants/${createdRestaurant.id}`);
+
+    // Then
+    expect(res1.status).toBe(HttpStatus.NOT_FOUND);
+
+    // When
+    const res2 = await agent.get(`${PATH}/restaurants`);
+
+    // Then
+    expect(res2.status).toBe(HttpStatus.OK);
+    expect(res2.body.data).toHaveLength(0);
   });
 
   it('GET /dishes', async () => {
