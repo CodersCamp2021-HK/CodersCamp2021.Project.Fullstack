f7571076cf9c49a734d9570c899afdc8f1f84d0e
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-22 21:39:29 +0100

    feat(server): limit city search to supported cities

--- packages/server/src/shared/api/decorators/ParamDishesFilterDecorator.ts
+++ packages/server/src/shared/api/decorators/ParamDishesFilterDecorator.ts
@@ -1,9 +1,10 @@
+import { OperationalCities } from '../../../restaurants/database';
 import { DishTags, MealType } from '../../../restaurants/dishes/database';
 import { QueryFilters } from './QueryFiltersDecorator';
 
 const ParamDishesFilter = () =>
   QueryFilters([
-    { name: 'city', required: false },
+    { name: 'city', required: false, enum: OperationalCities, enumName: 'OperationalCityEnum' },
     { name: 'mealType', required: false, enum: MealType, enumName: 'MealTypeEnum', isArray: true },
     { name: 'tags', required: false, enum: DishTags, enumName: 'DishTagEnum', isArray: true },
   ]);
--- packages/server/src/shared/api/decorators/ParamRestaurantsFilterDecorator.ts
+++ packages/server/src/shared/api/decorators/ParamRestaurantsFilterDecorator.ts
@@ -1,9 +1,9 @@
-import { CuisineTypes, RestaurantTags } from '../../../restaurants/database';
+import { CuisineTypes, OperationalCities, RestaurantTags } from '../../../restaurants/database';
 import { QueryFilters } from './QueryFiltersDecorator';
 
 const ParamRestaurantsFilter = () =>
   QueryFilters([
-    { name: 'city', required: false },
+    { name: 'city', required: false, enum: OperationalCities, enumName: 'OperationalCityEnum' },
     { name: 'cuisineType', required: false, enum: CuisineTypes, enumName: 'CuisineTypeEnum', isArray: true },
     { name: 'tags', required: false, enum: RestaurantTags, enumName: 'RestaurantTagEnum', isArray: true },
   ]);
