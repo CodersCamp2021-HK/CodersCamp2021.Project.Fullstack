c15497744aa2c9c1e5ee862832fc183ebfcc289d
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-03-29 18:30:03 +0200

    fix(server): fix incorrect order submission logic
    Now, user is required to have completed profile to order, also ordered
    dishes need to be valid.

--- packages/server/src/orders/domain/CreateOrderHandler.ts
+++ packages/server/src/orders/domain/CreateOrderHandler.ts
@@ -5,8 +5,9 @@ import { Model } from 'mongoose';
 
 import { AuthFacade } from '../../auth/infra/AuthFacade';
 import { OrderMailService } from '../../mail';
+import { DishesFacade } from '../../restaurants';
 import { Handler } from '../../shared';
-import { User, UserDocument } from '../../users/database';
+import { UsersFacade } from '../../users';
 import { Order, OrderDocument } from '../database';
 
 interface CreateOrderRequest {
@@ -28,22 +29,29 @@ interface CreateOrderRequest {
 class CreateOrderHandler implements Handler<CreateOrderRequest, Order> {
   constructor(
     @InjectModel(Order.name) private orderModel: Model<OrderDocument>,
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private readonly usersFacade: UsersFacade,
     private readonly authFacade: AuthFacade,
+    private readonly dishesFacade: DishesFacade,
     private readonly orderMailService: OrderMailService,
   ) {}
 
   async exec(req: CreateOrderRequest): Promise<Order> {
-    const user = await this.userModel.findById(req.userId);
-    if (user?.isCompleted) {
-      const created = await this.orderModel.create({ ...req, date: new Date() });
-      const mail = await this.authFacade.getEmailByUserId(req.userId);
+    const userCanOrder = await this.usersFacade.canOrder(req.userId);
+    if (!userCanOrder) throw new UnprocessableEntityException("This user can't order a dish!");
 
-      if (mail != null) {
-        await this.orderMailService.sendOrderConfirmation(mail, created.id);
-      }
-      return plainToInstance(Order, created);
-    } else throw new UnprocessableEntityException();
+    const dishesAreValid = await this.dishesFacade.areOrderable(
+      req.subOrders.flatMap((suborder) => suborder.dishes.map((dish) => dish.dishId)),
+    );
+    if (!dishesAreValid) throw new UnprocessableEntityException('Invalid dishes supplied!');
+
+    const created = await this.orderModel.create({ ...req, date: new Date() });
+
+    const mail = await this.authFacade.getEmailByUserId(req.userId);
+    if (mail != null) {
+      await this.orderMailService.sendOrderConfirmation(mail, created.id);
+    }
+
+    return plainToInstance(Order, created);
   }
 }
 
