8219434e962c4701c42f84180437f855fd93213e
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-04-08 17:11:08 +0200

    test(client): fix client test after removing stub

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,37 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import { App } from './App';
 
-const fetchMock = jest.fn();
-window.fetch = fetchMock;
-
 describe('<App/>', () => {
-  it('should', async () => {
-    fetchMock.mockResolvedValueOnce({
-      status: 200,
-      ok: true,
-      json: async () => ({
-        data: [
-          { id: '123', cuisineType: [], tags: [], addressId: [] },
-          { id: '135', cuisineType: [], tags: [], addressId: [] },
-        ],
-        pages: 12,
-      }),
-    });
-
+  it('should render Home page at launch', async () => {
     // Given
     render(<App />);
 
     // Then
-    expect(await screen.findByText(/"id":"123"/)).toBeInTheDocument();
-
-    // When
-    const btn = screen.getByRole(/button/i, { name: /count is: 0/i });
-    userEvent.click(btn);
-
-    // Then
-    expect(screen.queryByRole(/button/i, { name: /count is: 0/i })).not.toBeInTheDocument();
-    expect(screen.getByRole(/button/i, { name: /count is: 1/i })).toBeInTheDocument();
+    expect(await screen.findByText(/Złóż zamówienie/)).toBeInTheDocument();
   });
 });
