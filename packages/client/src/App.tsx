ef64bd3d081cbf78eafbda39152282e46e8068d3
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-04-10 16:40:18 +0200

    add basic switcher

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,29 +1,32 @@
 import './App.css';
 
 import { Configuration, RestaurantDto, RestaurantsApi } from '@fullstack/sdk';
-import { Button } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 
 import logo from './logo.svg';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1b5e20',
-    },
-    secondary: {
-      main: '#cddc39',
-    },
-    background: {
-      default: '#F4F3DD',
-    },
-  },
-  typography: {
-    fontFamily: 'Noto Sans',
-    fontSize: 14,
-  },
-});
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#1b5e20',
+//     },
+//     secondary: {
+//       main: '#cddc39',
+//     },
+//     background: {
+//       default: '#F4F3DD',
+//     },
+//   },
+//   typography: {
+//     fontFamily: 'Noto Sans',
+//     fontSize: 14,
+//   },
+// });
 
 const PROD_API_BASE_PATH = 'https://coderscamp2021-hk-fullstack.herokuapp.com';
 const DEV_API_BASE_PATH = 'http://localhost:4000';
@@ -38,7 +41,16 @@ const api = new RestaurantsApi(
 );
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [themeMode, setThemeMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: themeMode ? 'dark' : 'light',
+    },
+  });
+
+  const toggleDarkMode = () => setThemeMode(!themeMode);
+  // const [count, setCount] = useState(0);
 
   const [data, setData] = useState<RestaurantDto[]>([]);
 
@@ -50,7 +62,7 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className='App'>
+      <div className='App' styles={ backgroundColor: 'background.default'}>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <ul>
@@ -59,11 +71,23 @@ const App = () => {
             ))}
           </ul>
           <p>Hello Vite + React!</p>
-          <p>
-            <Button variant='contained' onClick={() => setCount((x) => x + 1)}>
-              count is: {count}
-            </Button>
-          </p>
+          <Box
+            sx={{
+              display: 'flex',
+              width: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              bgcolor: 'background.default',
+              color: 'text.primary',
+              borderRadius: 1,
+              p: 3,
+            }}
+          >
+            {theme.palette.mode} mode
+            <IconButton sx={{ ml: 1 }} onClick={toggleDarkMode} color='inherit'>
+              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Box>
           <p>
             Edit <code>App.tsx</code> and save to test HMR updates.
           </p>
