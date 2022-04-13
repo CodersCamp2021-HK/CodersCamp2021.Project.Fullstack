8095a4bc5ff945d4aa07f976a5089a2abba331d5
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-04-13 23:51:26 +0200

    refactor(client): separate PAGES into left & right parts

--- packages/client/src/components/AppNavBar.tsx
+++ packages/client/src/components/AppNavBar.tsx
@@ -1,66 +1,51 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasketOutlined';
-import {
-  AppBar,
-  Box,
-  Button,
-  Fab,
-  IconButton,
-  Link,
-  Menu,
-  MenuItem,
-  Palette,
-  Theme,
-  Toolbar,
-  useTheme,
-} from '@mui/material';
+import { AppBar, Box, Button, Fab, IconButton, Link, Menu, MenuItem, Theme, Toolbar } from '@mui/material';
 import { useState } from 'react';
 
 import logo from '../assets/logo.svg';
 
-const PAGES = [
+const LEFT_PAGES = [
   {
     name: 'O nas',
     href: 'about',
-    pos: 'left',
   },
   {
     name: 'Kontakt',
     href: 'contact',
-    pos: 'left',
   },
   {
     name: 'Dostawa',
     href: 'delivery',
-    pos: 'left',
   },
+] as const;
+
+const RIGHT_PAGES = [
   {
     name: 'Logowanie',
     href: 'login',
-    pos: 'right',
-    color: (palette: Palette) => palette.primary.main,
+    color: (theme: Theme) => theme.palette.primary.main,
   },
   {
     name: 'Rejestracja',
     href: 'register',
-    pos: 'right',
-    color: (palette: Palette) => palette.secondary.dark,
+    color: (theme: Theme) => theme.palette.secondary.dark,
   },
 ] as const;
 
-const pageToButton = (page: typeof PAGES[number], theme: Theme) => (
+const PAGES = [...LEFT_PAGES, ...RIGHT_PAGES] as const;
+
+const pageToButton = (page: typeof PAGES[number]) => (
   <Button
     key={page.href}
     href={page.href}
-    sx={{ color: 'color' in page ? page.color(theme.palette) : theme.palette.secondary.contrastText, px: 2 }}
+    sx={{ color: 'color' in page ? page.color : (theme) => theme.palette.secondary.contrastText, px: 2 }}
   >
     {page.name}
   </Button>
 );
 
 const AppNavBar = () => {
-  const theme = useTheme();
-
   const [menuAnchorElem, setMenuAnchorElem] = useState<null | HTMLElement>(null);
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
@@ -97,12 +82,8 @@ const AppNavBar = () => {
         <Link href='/' sx={{ mx: 'auto' }}>
           <img src={logo} alt='JeszCoChcesz' style={{ display: 'block', height: '6rem', paddingBlock: '1rem' }} />
         </Link>
-        <Box sx={{ flexGrow: 1, ml: 8, display: { xs: 'none', md: 'block' } }}>
-          {PAGES.filter((page) => page.pos === 'left').map((page) => pageToButton(page, theme))}
-        </Box>
-        <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-          {PAGES.filter((page) => page.pos === 'right').map((page) => pageToButton(page, theme))}
-        </Box>
+        <Box sx={{ flexGrow: 1, ml: 8, display: { xs: 'none', md: 'block' } }}>{LEFT_PAGES.map(pageToButton)}</Box>
+        <Box sx={{ display: { xs: 'none', md: 'block' } }}>{RIGHT_PAGES.map(pageToButton)}</Box>
         <Fab href='/shoppingcart' color='secondary' sx={{ boxShadow: 0, ml: 2 }}>
           <ShoppingBasketIcon color='primary' />
         </Fab>
