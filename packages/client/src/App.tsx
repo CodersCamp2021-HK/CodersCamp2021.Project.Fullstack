060a31bbf2302d0b38100683222038c61731f656
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-04-23 16:20:39 +0200

    refactor(client): bring over some changes from issue-148-Implement_order_summary

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,27 +1,8 @@
-import { Configuration } from '@fullstack/sdk';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { routes } from './config';
 import { Home, Main } from './pages';
-
-const PROD_API_BASE_PATH = 'https://coderscamp2021-hk-fullstack.herokuapp.com';
-const DEV_API_BASE_PATH = 'http://localhost:4000';
-
-const mode = `import.meta.env.MODE` as string;
-const isProduction = mode === `"production"`;
-
-// TODO: Use the API
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const configuration = new Configuration({
-  basePath: isProduction ? PROD_API_BASE_PATH : DEV_API_BASE_PATH,
-});
-
-// const api = new RestaurantsApi(
-//   new Configuration({
-//     basePath: isProduction ? PROD_API_BASE_PATH : DEV_API_BASE_PATH,
-//   }),
-// );
 
 const App = () => {
   return (
--- packages/client/src/config/index.ts
+++ packages/client/src/config/index.ts
@@ -1,2 +1,3 @@
+export * from './apiConfiguration';
 export * from './routes';
 export * from './theme';
