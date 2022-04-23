cb88187e56465d20e5a61a337ff9e2fc08118012
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-04-23 13:46:07 +0200

    add filterrs component

--- packages/client/src/components/Filters/Filters.tsx
+++ packages/client/src/components/Filters/Filters.tsx
@@ -1,4 +1,4 @@
-import Box from '@mui/material/Box';
+import { CuisineTypeEnum, DishTagsEnum, MealTypeEnum } from '@fullstack/sdk';
 import Checkbox from '@mui/material/Checkbox';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -9,23 +9,22 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import { useState } from 'react';
 
-interface FilterGroupProps {
-  name: string;
-  inputGroup: string[];
-}
-
-const Filters = ({ name, inputGroup }: FilterGroupProps) => {
-  return (
-    <FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
-      <FormLabel component='legend'>{name}</FormLabel>
-      <FormGroup>
-        {inputGroup.map((type) => {
-          return <FormControlLabel key={`${name}-${type}`} control={<Checkbox />} label={type} />;
-        })}
-      </FormGroup>
-    </FormControl>
-  );
+type FiltersProps = {
+  listLabel: string;
+  filters: DishTagsEnum | CuisineTypeEnum | MealTypeEnum;
 };
+
+const CheckboxList = ({ listLabel, filters }: FiltersProps) => (
+  <FormControl sx={{ m: 3 }} component='fieldset' variant='standard'>
+    <FormLabel component='legend'>{listLabel}</FormLabel>
+    <FormGroup>
+      {Object.keys(filters).map((key: string) => {
+        return <FormControlLabel key={key} control={<Checkbox />} label={filters[key]} />;
+      })}
+    </FormGroup>
+  </FormControl>
+);
+
 const BasicSelect = () => {
   const [sort, setSort] = useState('');
 
@@ -34,22 +33,20 @@ const BasicSelect = () => {
   };
 
   return (
-    <Box>
-      <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id='demo-simple-select-label'>Sortuj</InputLabel>
-        <Select
-          labelId='demo-simple-select-label'
-          id='demo-simple-select'
-          value={sort}
-          label='Sortuj'
-          onChange={handleChange}
-        >
-          <MenuItem value={0}>wg. ceny rosnąco</MenuItem>
-          <MenuItem value={1}>wg. kaloryczności rosnąco</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+      <InputLabel id='demo-simple-select-label'>Sortuj</InputLabel>
+      <Select
+        labelId='demo-simple-select-label'
+        id='demo-simple-select'
+        value={sort}
+        label='Sortuj'
+        onChange={handleChange}
+      >
+        <MenuItem value={0}>wg. ceny rosnąco</MenuItem>
+        <MenuItem value={1}>wg. kaloryczności rosnąco</MenuItem>
+      </Select>
+    </FormControl>
   );
 };
 
-export { BasicSelect, Filters };
+export { BasicSelect, CheckboxList };
--- packages/client/src/pages/Home.tsx
+++ packages/client/src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import { AppNavBar, Filters, Footer, Hero } from '../components';
+import { AppNavBar, Footer, Hero } from '../components';
 import { HomeSteps } from '../components/HomeSteps';
+
+// const drawerWidth = 320;
 
 const Home = () => (
   <>
     <AppNavBar />
     <Hero />
-    <Filters />
     <HomeSteps />
     <Footer />
   </>
