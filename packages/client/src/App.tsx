626a93c35041351a5d1fa4a5a1a2db251068ede6
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-04-23 18:15:43 +0200

    feat(client): add UnderConstruction page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppNavBar } from './components';
 import { routes } from './config';
 import { ThemeContextProvider } from './contexts';
-import { Home, Main } from './pages';
+import { Home, Main, UnderConstruction } from './pages';
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path={routes.home} element={<Home />} />
           <Route path={routes.main} element={<Main />} />
+          <Route path='*' element={<UnderConstruction />} />
         </Routes>
       </BrowserRouter>
     </ThemeContextProvider>
