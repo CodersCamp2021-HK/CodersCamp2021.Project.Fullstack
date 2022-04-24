172900f65ae563bc6183119fb2af5d5a300fa20b
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-04-24 23:06:24 +0200

    fix mistake

--- packages/client/src/components/MediaCardsGrid.tsx
+++ packages/client/src/components/MediaCardsGrid.tsx
@@ -1,127 +1,81 @@
 import Grid from '@mui/material/Grid';
 
+import cardImg from '../assets/default.png';
 import { MediaCard } from './MediaCard';
 
 const MediaCardsGrid = () => {
-  const dishes = {
-    data: [
-      {
-        id: '6200218668fc82e7bdf15088',
-        name: 'Danie 1',
-        photo: 'https://coderscamp2021-hk-fullstack.herokuapp.com/api/img/restaurant/6200218668fc82e7bdf15088',
-        mealType: ['lunch', 'obiad'],
-        description: 'Opis dania',
-        price: 23,
-        tags: ['ostre', 'gluten free'],
-        ingredients: [
-          {
-            name: 'bazylia',
-            canBeExcluded: false,
-          },
-        ],
-        allergens: ['orzechy'],
-        portionWeight: 0,
-        calories: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        fats: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        proteins: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        carbohydrates: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        restaurant: '6200218668fc82e7bdf15088',
-      },
-      {
-        id: '6200218668fc82e7bdf15088',
-        name: 'Danie 1',
-        photo: 'https://coderscamp2021-hk-fullstack.herokuapp.com/api/img/restaurant/6200218668fc82e7bdf15088',
-        mealType: ['lunch', 'obiad'],
-        description: 'Opis dania',
-        price: 25,
-        tags: ['ostre', 'gluten free'],
-        ingredients: [
-          {
-            name: 'bazylia',
-            canBeExcluded: false,
-          },
-        ],
-        allergens: ['orzechy'],
-        portionWeight: 0,
-        calories: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        fats: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        proteins: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        carbohydrates: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        restaurant: '6200218668fc82e7bdf15088',
-      },
-      {
-        id: '6200218668fc82e7bdf15088',
-        name: 'Danie 1',
-        photo: 'https://coderscamp2021-hk-fullstack.herokuapp.com/api/img/restaurant/6200218668fc82e7bdf15088',
-        mealType: ['lunch', 'obiad'],
-        description: 'Opis dania',
-        price: 20,
-        tags: ['ostre', 'gluten free'],
-        ingredients: [
-          {
-            name: 'bazylia',
-            canBeExcluded: false,
-          },
-        ],
-        allergens: ['orzechy'],
-        portionWeight: 0,
-        calories: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        fats: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        proteins: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        carbohydrates: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        restaurant: '6200218668fc82e7bdf15088',
-      },
-    ],
-    pages: 0,
-  };
-
-  const cardsGrid = dishes.data?.map((dish) => {
-    return (
-      <Grid item key={dish.id}>
-        <MediaCard dish={dish} />
-      </Grid>
-    );
-  });
-
   return (
     <Grid container spacing={2} pt={6}>
-      {cardsGrid}
+      <Grid item>
+        <MediaCard
+          dish={{
+            name: 'Bowl japoński',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            photo: cardImg,
+            price: 32,
+            calories: { perPortion: 550, per100g: 0 },
+            fats: { perPortion: 250, per100g: 0 },
+            proteins: { perPortion: 20, per100g: 0 },
+            carbohydrates: { perPortion: 20, per100g: 0 },
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <MediaCard
+          dish={{
+            name: 'Bowl japoński',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            photo: cardImg,
+            price: 32,
+            calories: { perPortion: 550, per100g: 0 },
+            fats: { perPortion: 250, per100g: 0 },
+            proteins: { perPortion: 20, per100g: 0 },
+            carbohydrates: { perPortion: 20, per100g: 0 },
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <MediaCard
+          dish={{
+            name: 'Bowl japoński',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            photo: cardImg,
+            price: 32,
+            calories: { perPortion: 550, per100g: 0 },
+            fats: { perPortion: 250, per100g: 0 },
+            proteins: { perPortion: 20, per100g: 0 },
+            carbohydrates: { perPortion: 20, per100g: 0 },
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <MediaCard
+          dish={{
+            name: 'Bowl japoński',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            photo: cardImg,
+            price: 32,
+            calories: { perPortion: 550, per100g: 0 },
+            fats: { perPortion: 250, per100g: 0 },
+            proteins: { perPortion: 20, per100g: 0 },
+            carbohydrates: { perPortion: 20, per100g: 0 },
+          }}
+        />
+      </Grid>
+      <Grid item>
+        <MediaCard
+          dish={{
+            name: 'Bowl japoński',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+            photo: cardImg,
+            price: 32,
+            calories: { perPortion: 550, per100g: 0 },
+            fats: { perPortion: 250, per100g: 0 },
+            proteins: { perPortion: 20, per100g: 0 },
+            carbohydrates: { perPortion: 20, per100g: 0 },
+          }}
+        />
+      </Grid>
     </Grid>
   );
 };
