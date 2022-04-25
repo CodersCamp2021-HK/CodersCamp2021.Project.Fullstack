7357692bada031979c95deb9cafd6989fd9d98cc
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-04-25 13:46:07 +0200

    feat(client): display dishes from the API

--- packages/client/src/components/MediaCard/MediaCardsGrid.tsx
+++ packages/client/src/components/MediaCard/MediaCardsGrid.tsx
@@ -1,110 +1,127 @@
+import { DishDto, DishesApi } from '@fullstack/sdk';
 import Grid from '@mui/material/Grid';
+import { useEffect, useState } from 'react';
 
+import { apiConfiguration } from '../../config';
 import { MediaCard } from './MediaCard';
 
-const MediaCardsGrid = () => {
-  const dishes = {
-    data: [
+const EXAMPLE_DISHES = [
+  {
+    id: '6200218668fc82e7bdf15088',
+    name: 'Danie 1',
+    mealType: ['lunch', 'obiad'],
+    description: 'Opis dania',
+    price: 23,
+    ingredients: [
       {
-        id: '6200218668fc82e7bdf15088',
-        name: 'Danie 1',
-        mealType: ['lunch', 'obiad'],
-        description: 'Opis dania',
-        price: 23,
-        ingredients: [
-          {
-            name: 'bazylia',
-            canBeExcluded: false,
-          },
-        ],
-        portionWeight: 0,
-        calories: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        fats: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        proteins: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        carbohydrates: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        restaurant: '6200218668fc82e7bdf15088',
-      },
-      {
-        id: '6200218668fc82e7bdf15088',
-        name: 'Danie 1',
-        photo: '',
-        mealType: ['lunch', 'obiad'],
-        description: 'Opis dania',
-        price: 25,
-        ingredients: [
-          {
-            name: 'bazylia',
-            canBeExcluded: false,
-          },
-        ],
-        portionWeight: 0,
-        calories: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        fats: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        proteins: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        carbohydrates: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        restaurant: '6200218668fc82e7bdf15088',
-      },
-      {
-        id: '6200218668fc82e7bdf15088',
-        name: 'Danie 1',
-        photo: '',
-        mealType: ['lunch', 'obiad'],
-        description: 'Opis dania',
-        price: 20,
-        ingredients: [
-          {
-            name: 'bazylia',
-            canBeExcluded: false,
-          },
-        ],
-        portionWeight: 0,
-        calories: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        fats: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        proteins: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        carbohydrates: {
-          per100g: 0,
-          perPortion: 0,
-        },
-        restaurant: '6200218668fc82e7bdf15088',
+        name: 'bazylia',
+        canBeExcluded: false,
       },
     ],
-    pages: 0,
-  };
+    portionWeight: 0,
+    calories: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    fats: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    proteins: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    carbohydrates: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    restaurant: '6200218668fc82e7bdf15088',
+  },
+  {
+    id: '6200218668fc82e7bdf15088',
+    name: 'Danie 1',
+    photo: '',
+    mealType: ['lunch', 'obiad'],
+    description: 'Opis dania',
+    price: 25,
+    ingredients: [
+      {
+        name: 'bazylia',
+        canBeExcluded: false,
+      },
+    ],
+    portionWeight: 0,
+    calories: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    fats: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    proteins: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    carbohydrates: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    restaurant: '6200218668fc82e7bdf15088',
+  },
+  {
+    id: '6200218668fc82e7bdf15088',
+    name: 'Danie 1',
+    photo: '',
+    mealType: ['lunch', 'obiad'],
+    description: 'Opis dania',
+    price: 20,
+    ingredients: [
+      {
+        name: 'bazylia',
+        canBeExcluded: false,
+      },
+    ],
+    portionWeight: 0,
+    calories: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    fats: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    proteins: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    carbohydrates: {
+      per100g: 0,
+      perPortion: 0,
+    },
+    restaurant: '6200218668fc82e7bdf15088',
+  },
+];
 
-  const cardsGrid = dishes.data?.map((dish) => {
+const dishesApi = new DishesApi(apiConfiguration);
+
+const MediaCardsGrid = () => {
+  const [dishes, setDishes] = useState<DishDto[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const dishReponse = await dishesApi.listAllDishes();
+        setDishes(dishReponse.data);
+      } catch (error) {
+        setDishes(EXAMPLE_DISHES);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const cardsGrid = dishes.map((dish) => {
     return (
       <Grid item key={dish.id}>
         <MediaCard dish={dish} />
