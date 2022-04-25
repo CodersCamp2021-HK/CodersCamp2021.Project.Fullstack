426fb3e7f067b3a5d8514b848605f0d80483f9f1
mmejer <44590421+mmejer@users.noreply.github.com>
2022-04-25 17:23:55 +0200

    fix(client): merge duplicated main pages

--- packages/client/src/pages/Main/Main.tsx
+++ packages/client/src/pages/Main/Main.tsx
@@ -1,7 +1,8 @@
+import { Stack } from '@mui/material';
 import Container from '@mui/material/Container';
 import { SetStateAction, useState } from 'react';
 
-import { MediaCardsGrid } from '../../components';
+import { BasicSelect, Filters, MediaCardsGrid } from '../../components';
 import { CartSummary } from '../../components/CartSummary';
 
 const Main = () => {
@@ -10,10 +11,38 @@ const Main = () => {
     setDay(e);
   };
 
+  const sortObj = {
+    priceAsc: 'wg. ceny rosnąco',
+    kcalAsc: 'wg. kaloryczności rosnąco',
+  };
+
   return (
-    <Container maxWidth='xl' sx={{ display: 'flex' }}>
-      <MediaCardsGrid />
-      <CartSummary day={day} onDayChange={handleChange} />
+    <Container maxWidth={false}>
+      <Stack sx={{ flex: 1 }} direction='row'>
+        <Stack
+          spacing={2}
+          sx={{
+            maxWidth: 280,
+            width: '100%',
+            position: 'sticky',
+            top: '0',
+            height: '100vh',
+            overflowY: 'scroll',
+            justifyContent: 'flex-start',
+            pr: 1,
+            pb: 6,
+            mt: 6,
+            mr: 4,
+          }}
+        >
+          <BasicSelect label='Sortuj' selectObj={sortObj} />
+          <Stack spacing={2}>
+            <Filters />
+          </Stack>
+        </Stack>
+        <MediaCardsGrid />
+        <CartSummary day={day} onDayChange={handleChange} />
+      </Stack>
     </Container>
   );
 };
