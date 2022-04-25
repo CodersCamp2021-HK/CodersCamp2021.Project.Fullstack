0882be6f1393eca1d3c9381736fe278ff6d86426
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-04-25 14:01:31 +0200

    updated unused component

--- packages/client/src/pages/Main.tsx
+++ packages/client/src/pages/Main.tsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mui/material';
 import Container from '@mui/material/Container';
 
-import { AppNavBar, Filters, MediaCardsGrid } from '../components';
+import { Filters, MediaCardsGrid } from '../components';
 import { BasicSelect } from '../components/Filters';
 
 const Main = () => {
@@ -11,35 +11,33 @@ const Main = () => {
   };
 
   return (
-    <>
-      <Container maxWidth={false}>
-        <Stack sx={{ flex: 1 }} direction='row'>
-          <Stack
-            spacing={2}
-            sx={{
-              maxWidth: 280,
-              width: '100%',
-              position: 'sticky',
-              top: '0',
-              height: '100vh',
-              overflowY: 'scroll',
-              justifyContent: 'flex-start',
-              pr: 1,
-              pb: 6,
-              mt: 6,
-              mr: 4,
-            }}
-          >
-            <BasicSelect label='Sortuj' selectObj={sortObj} />
-            <Stack spacing={2}>
-              <Filters />
-            </Stack>
+    <Container maxWidth={false}>
+      <Stack sx={{ flex: 1 }} direction='row'>
+        <Stack
+          spacing={2}
+          sx={{
+            maxWidth: 280,
+            width: '100%',
+            position: 'sticky',
+            top: '0',
+            height: '100vh',
+            overflowY: 'scroll',
+            justifyContent: 'flex-start',
+            pr: 1,
+            pb: 6,
+            mt: 6,
+            mr: 4,
+          }}
+        >
+          <BasicSelect label='Sortuj' selectObj={sortObj} />
+          <Stack spacing={2}>
+            <Filters />
           </Stack>
-
-          <MediaCardsGrid />
         </Stack>
-      </Container>
-    </>
+
+        <MediaCardsGrid />
+      </Stack>
+    </Container>
   );
 };
 
