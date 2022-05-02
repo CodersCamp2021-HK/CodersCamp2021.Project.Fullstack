9b242c395b76e0761bc548290bb0ea8904392b0e
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-05-02 11:31:14 +0200

    feat(server): filter dishes by cuisineType

--- packages/server/src/shared/api/decorators/ParamDishesFilterDecorator.ts
+++ packages/server/src/shared/api/decorators/ParamDishesFilterDecorator.ts
@@ -1,10 +1,11 @@
-import { OperationalCities } from '../../../restaurants/database';
+import { CuisineTypes, OperationalCities } from '../../../restaurants/database';
 import { DishTags, MealType } from '../../../restaurants/dishes/database';
 import { QueryFilters } from './QueryFiltersDecorator';
 
 const ParamDishesFilter = () =>
   QueryFilters([
     { name: 'city', required: false, enum: OperationalCities, enumName: 'OperationalCityEnum' },
+    { name: 'cuisineType', required: false, enum: CuisineTypes, enumName: 'CuisineTypeEnum', isArray: true },
     { name: 'mealType', required: false, enum: MealType, enumName: 'MealTypeEnum', isArray: true },
     { name: 'tags', required: false, enum: DishTags, enumName: 'DishTagEnum', isArray: true },
   ]);
