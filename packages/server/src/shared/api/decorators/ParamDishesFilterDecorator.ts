9ae978c6ac5a559ac621915c676306d161c6a6df
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-05-06 17:53:34 +0200

    fix(server): apply duplicate enum fix from #194

--- packages/server/src/shared/api/decorators/ParamDishesFilterDecorator.ts
+++ packages/server/src/shared/api/decorators/ParamDishesFilterDecorator.ts
@@ -4,9 +4,9 @@ import { QueryFilters } from './QueryFiltersDecorator';
 
 const ParamDishesFilter = () =>
   QueryFilters([
-    { name: 'city', required: false, enum: OperationalCities, enumName: 'OperationalCityEnum' },
-    { name: 'mealType', required: false, enum: MealType, enumName: 'MealTypeEnum', isArray: true },
-    { name: 'tags', required: false, enum: DishTags, enumName: 'DishTagEnum', isArray: true },
+    { name: 'city', required: false, enum: OperationalCities, enumName: 'QOperationalCityEnum' },
+    { name: 'mealType', required: false, enum: MealType, enumName: 'QMealTypeEnum', isArray: true },
+    { name: 'tags', required: false, enum: DishTags, enumName: 'QDishTagsEnum', isArray: true },
   ]);
 
 export { ParamDishesFilter };
--- packages/server/src/shared/api/decorators/ParamRestaurantsFilterDecorator.ts
+++ packages/server/src/shared/api/decorators/ParamRestaurantsFilterDecorator.ts
@@ -3,9 +3,9 @@ import { QueryFilters } from './QueryFiltersDecorator';
 
 const ParamRestaurantsFilter = () =>
   QueryFilters([
-    { name: 'city', required: false, enum: OperationalCities, enumName: 'OperationalCityEnum' },
-    { name: 'cuisineType', required: false, enum: CuisineTypes, enumName: 'CuisineTypeEnum', isArray: true },
-    { name: 'tags', required: false, enum: RestaurantTags, enumName: 'RestaurantTagEnum', isArray: true },
+    { name: 'city', required: false, enum: OperationalCities, enumName: 'QOperationalCityEnum' },
+    { name: 'cuisineType', required: false, enum: CuisineTypes, enumName: 'QCuisineTypeEnum', isArray: true },
+    { name: 'tags', required: false, enum: RestaurantTags, enumName: 'QRestaurantTagEnum', isArray: true },
   ]);
 
 export { ParamRestaurantsFilter };
