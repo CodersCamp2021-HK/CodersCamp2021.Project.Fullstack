ee9a055ded655a128311ae7f9117dcea777facef
Kamil <37578394+KamilDudek@users.noreply.github.com>
2022-05-08 09:35:22 +0200

    Issue 188 implement data completion page second shopping card subpage (#197)
    * feat: add subpage shopping cart, data completion
    
    * style: change margin
    
    * refactor: change input label size
    
    * style: change margin
    
    * feat: add test and storybook
    
    * refactor: minor changes

--- packages/client/src/pages/index.ts
+++ packages/client/src/pages/index.ts
@@ -2,4 +2,5 @@ export * from './Home';
 export * from './Main';
 export * from './RegisterAndLogin';
 export * from './ShoppingCart';
+export * from './ShoppingCartData';
 export * from './UnderConstruction';
