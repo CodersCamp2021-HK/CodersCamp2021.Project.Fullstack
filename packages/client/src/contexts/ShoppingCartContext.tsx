ee4ab83497f50ab13f6651ad36417b81d46b0b42
tchojnacki <tomaszchojnacki2001@gmail.com>
2022-05-15 21:01:38 +0200

    refactor(client): simplity ShoppingCart operations

--- packages/client/src/contexts/ShoppingCartContext.tsx
+++ packages/client/src/contexts/ShoppingCartContext.tsx
@@ -1,6 +1,5 @@
 import { DishDto, OrderDishDto } from '@fullstack/sdk';
 import produce from 'immer';
-import { isEqual } from 'lodash';
 import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
 
 type SubOrderDish = Omit<OrderDishDto, 'dishId'> & { dish: DishDto };
@@ -12,9 +11,13 @@ interface SubOrder {
 const orderDishKey = (orderDish: SubOrderDish) =>
   `${orderDish.dish.id}:${JSON.stringify(orderDish.excludedIngredients ?? [])}"`;
 
-const areDishesEqual = (suborderDish1: SubOrderDish, suborderDish2: SubOrderDish) =>
-  suborderDish1.dish.id === suborderDish2.dish.id &&
-  isEqual(suborderDish1.excludedIngredients, suborderDish2.excludedIngredients);
+const getDayDishes = (draft: SubOrder[], date: Date | null) =>
+  draft.find(({ deliveryDate }) => deliveryDate === date)?.dishes;
+
+const getTargetDish = (dayDishes: SubOrderDish[], suborderDish: SubOrderDish) => {
+  const idx = dayDishes.findIndex((foundDish) => orderDishKey(foundDish) === orderDishKey(suborderDish));
+  return [idx, idx === -1 ? undefined : dayDishes[idx]] as const;
+};
 
 const ShoppingCartContext = createContext({
   cart: [] as SubOrder[],
@@ -34,73 +37,76 @@ const ShoppingCartProvider = ({ children }: { children: ReactNode }) => {
     (suborderDish: SubOrderDish, date: Date | null = selectedDate) =>
       setCart(
         produce((draft) => {
-          if (date === null) return;
-
-          const suborder = draft.find(({ deliveryDate }) => deliveryDate === date);
-
-          if (!suborder) {
-            draft.push({ deliveryDate: date, dishes: [suborderDish] });
+          const dayDishes = getDayDishes(draft, date);
+          if (!dayDishes) {
+            draft.push({ deliveryDate: date as Date, dishes: [suborderDish] });
             return;
           }
 
-          const duplicateDish = suborder.dishes.find((foundDish) => areDishesEqual(foundDish, suborderDish));
+          const [, duplicateDish] = getTargetDish(dayDishes, suborderDish);
           if (duplicateDish) {
             duplicateDish.count = (duplicateDish.count ?? 1) + (suborderDish.count ?? 1);
-            return;
+          } else {
+            dayDishes.push(suborderDish);
           }
-
-          suborder.dishes.push(suborderDish);
         }),
       ),
     [selectedDate],
   );
 
-  const modifyDishCount = useCallback((suborderDish: SubOrderDish, date: Date, modifier: (prev: number) => number) => {
-    setCart(
-      produce((draft) => {
-        const targetDish = draft
-          .find(({ deliveryDate }) => deliveryDate === date)
-          ?.dishes?.find((foundDish) => areDishesEqual(foundDish, suborderDish));
+  const modifyDishCount = useCallback(
+    (suborderDish: SubOrderDish, date: Date, modifier: (prev: number) => number) =>
+      setCart(
+        produce((draft) => {
+          const dayDishes = getDayDishes(draft, date);
+          if (!dayDishes) return;
 
-        if (!targetDish) return;
+          const [, targetDish] = getTargetDish(dayDishes, suborderDish);
+          if (!targetDish) return;
 
-        targetDish.count = modifier(targetDish.count ?? 1);
-      }),
-    );
-  }, []);
+          targetDish.count = modifier(targetDish.count ?? 1);
+        }),
+      ),
+    [],
+  );
 
-  const removeFromCart = useCallback((suborderDish: SubOrderDish, date: Date) => {
-    setCart(
-      produce((draft) => {
-        const dayDishes = draft.find(({ deliveryDate }) => deliveryDate === date)?.dishes;
-        if (!dayDishes) return;
+  const removeFromCart = useCallback(
+    (suborderDish: SubOrderDish, date: Date) =>
+      setCart(
+        produce((draft) => {
+          const dayDishes = getDayDishes(draft, date);
+          if (!dayDishes) return;
 
-        const targetIndex = dayDishes.findIndex((foundDish) => areDishesEqual(foundDish, suborderDish));
-        if (targetIndex === -1) return;
+          const [targetIndex, targetDish] = getTargetDish(dayDishes, suborderDish);
+          if (!targetDish) return;
 
-        dayDishes.splice(targetIndex, 1);
+          dayDishes.splice(targetIndex, 1);
+          if (dayDishes.length === 0) {
+            draft.splice(
+              draft.findIndex(({ deliveryDate }) => deliveryDate === date),
+              1,
+            );
+          }
+        }),
+      ),
+    [],
+  );
 
-        if (dayDishes.length === 0) {
-          draft.splice(
-            draft.findIndex(({ deliveryDate }) => deliveryDate === date),
-            1,
-          );
-        }
-      }),
-    );
-  }, []);
+  const editInCart = useCallback(
+    (oldSuborderDish: SubOrderDish, newSuborderDish: SubOrderDish, date: Date) =>
+      setCart(
+        produce((draft) => {
+          const dayDishes = getDayDishes(draft, date);
+          if (!dayDishes) return;
 
-  const editInCart = useCallback((oldSuborderDish: SubOrderDish, newSuborderDish: SubOrderDish, date: Date) => {
-    setCart(
-      produce((draft) => {
-        const dayDishes = draft.find(({ deliveryDate }) => deliveryDate === date)?.dishes;
-        if (!dayDishes) return;
-        const targetIndex = dayDishes.findIndex((foundDish) => areDishesEqual(foundDish, oldSuborderDish));
-        if (targetIndex === -1) return;
-        dayDishes[targetIndex] = newSuborderDish;
-      }),
-    );
-  }, []);
+          const [targetIndex, targetDish] = getTargetDish(dayDishes, oldSuborderDish);
+          if (!targetDish) return;
+
+          dayDishes[targetIndex] = newSuborderDish;
+        }),
+      ),
+    [],
+  );
 
   const value = useMemo(
     () => ({ cart, addToCart, selectedDate, setSelectedDate, modifyDishCount, removeFromCart, editInCart }),
