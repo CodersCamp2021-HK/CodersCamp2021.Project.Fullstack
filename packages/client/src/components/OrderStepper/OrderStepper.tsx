c865ad62e3aeea6b5671059a83ba2e54683f7048
Justyna Skrajna <justynaskrajna@Justynas-MacBook-Pro.local>
2022-05-15 20:28:55 +0200

    add redirect to homepage when theres no item in cart

--- packages/client/src/components/OrderStepper/OrderStepper.tsx
+++ packages/client/src/components/OrderStepper/OrderStepper.tsx
@@ -1,19 +1,32 @@
 import { Box, Step, StepButton, StepLabel, Stepper } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { routes } from '../../config';
+import { useShoppingCart } from '../../contexts';
 
 const STEPS = ['Podsumowanie', 'Uzupełnij dane', 'Zapłać i zamów'] as const;
 
+const generateLabels = (label: string) => {
+  if (label === STEPS[0]) {
+    return <StepButton href={routes.shoppingCart}>{label}</StepButton>;
+  }
+  if (label === STEPS[1]) {
+    return <StepButton href={routes.shoppingCartData}>{label}</StepButton>;
+  }
+  return <StepLabel>{label}</StepLabel>;
+};
+
 const OrderStepper = ({ activeStepIndex }: { activeStepIndex: number }) => {
-  const generateLabels = (label: string) => {
-    if (label === STEPS[0]) {
-      return <StepButton href={routes.shoppingCart}>{label}</StepButton>;
+  const { cart } = useShoppingCart();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cart.length === 0) {
+      navigate(routes.home, { replace: true });
     }
-    if (label === STEPS[1]) {
-      return <StepButton href={routes.shoppingCartData}>{label}</StepButton>;
-    }
-    return <StepLabel>{label}</StepLabel>;
-  };
+  }, [cart, navigate]);
+
   return (
     <Box minWidth='70vw' marginX='auto'>
       <Stepper alternativeLabel activeStep={activeStepIndex}>
