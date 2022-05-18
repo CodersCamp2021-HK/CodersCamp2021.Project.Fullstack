2d60ce631edd8704c09dca1e18b107ffedbdf232
mmejer <44590421+mmejer@users.noreply.github.com>
2022-05-18 23:30:23 +0200

    feat(client): connect api

--- packages/client/src/components/OrderPayment/OrderPayment.tsx
+++ packages/client/src/components/OrderPayment/OrderPayment.tsx
@@ -1,5 +1,8 @@
+import { UserDto, UserssProfileApi } from '@fullstack/sdk';
 import { Box, Button, Container, Divider, Grid, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { apiConfiguration } from '../../config';
 
 const CARD_REGEX =
   /^(?:4[0-9]{12}(?:[0-9]{3})?)|(?:3[47][0-9]{13})|(?:5[1-5][0-9]{14})|(?:6(?:011|5[0-9][0-9])[0-9]{12})|(?:(?:2131|1800|35\d{3})\d{11})|(?:3(?:0[0-5]|[68][0-9])[0-9]{11})$/;
@@ -7,30 +10,48 @@ const DATE_REGEX = /([0-9]{4})-(?:[0-9]{2})-([0-9]{2})/;
 const CODE_REGEX = /^\d{3,4}$/;
 const CHARACTER_LIMIT = 300;
 
-const cardInfo = {
-  id: '1',
-  number: '4562574783836030',
-  expirationDate: '2022-10-12',
-  securityCode: '123',
-};
+const userApi = new UserssProfileApi(apiConfiguration);
 
 const OrderPayment = () => {
   const [comment, setComment] = useState('');
 
-  const [cardNumber, setCardNumber] = useState(cardInfo.number || '');
+  const [cardNumber, setCardNumber] = useState('');
   const cardNumberErrorMessage = cardNumber.match(CARD_REGEX) ? '' : 'Wpisz poprawny numer karty.';
 
-  const [expirationDate, setExpirationDate] = useState(cardInfo.expirationDate || '');
+  const [expirationDate, setExpirationDate] = useState('');
   const expirationDateErrorMessage = expirationDate.match(DATE_REGEX) ? '' : 'Wpisz poprawną datę ważności karty.';
 
-  const [securityCode, setSecurityCode] = useState(cardInfo.securityCode || '');
-  const securityCodeErrorMessage = securityCode.match(CODE_REGEX) ? '' : 'Wpisz poprawny kod.';
+  const [securityCode, setSecurityCode] = useState('');
+  const securityCodeErrorMessage = securityCode?.match(CODE_REGEX) ? '' : 'Wpisz poprawny kod.';
 
   const [didSubmit, setDidSubmit] = useState(false);
+  const [userData, setUserData] = useState<UserDto | undefined>();
+  const [cardDataEdited, setCardDataEdited] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await userApi.findById();
+      if (response) setUserData(response);
+      if (response.card) {
+        setCardNumber(response.card?.number);
+        setExpirationDate(response.card.expirationDate);
+        setSecurityCode(response.card.securityCode);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const editCardData = async (user: UserDto) => {
+    await userApi.update({ updateUserDto: user });
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setDidSubmit(true);
+    if (userData && cardDataEdited) {
+      userData.card = { number: cardNumber, expirationDate, securityCode };
+      editCardData(userData);
+    }
   };
 
   return (
@@ -92,6 +113,7 @@ const OrderPayment = () => {
                       helperText={cardNumberErrorMessage}
                       onChange={(e) => {
                         setCardNumber(e.target.value);
+                        setCardDataEdited(true);
                       }}
                     />
                     <TextField
@@ -105,6 +127,7 @@ const OrderPayment = () => {
                       helperText={expirationDateErrorMessage}
                       onChange={(e) => {
                         setExpirationDate(e.target.value);
+                        setCardDataEdited(true);
                       }}
                     />
                     <TextField
@@ -118,6 +141,7 @@ const OrderPayment = () => {
                       helperText={securityCodeErrorMessage}
                       onChange={(e) => {
                         setSecurityCode(e.target.value);
+                        setCardDataEdited(true);
                       }}
                     />
                   </Box>
